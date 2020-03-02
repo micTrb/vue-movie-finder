c563f7ff12a85e55ce25f31f882ac3bc0e2ac366
Michele T <aedonem@gmail.com>
2020-03-02 13:07:35 +0100

    Init movie app

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'app'
 }
 </script>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,4 +21,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
